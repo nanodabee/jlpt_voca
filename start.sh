27c810d89a7b5ea8a64bb2b4f19d20c1f04c763f
nanodabee <honeyblues26@gmail.com>
2026-08-10 17:52:28 +0900

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cd "$(dirname "$0")"
-python3 -m pip install -r requirements.txt
-echo "Open http://127.0.0.1:8000"
-python3 -m uvicorn main:app --host 127.0.0.1 --port 8000
